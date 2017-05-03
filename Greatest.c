fbd2c266b84e8dc631491a84cf94987cd0daead5
Samwel Opiyo <samonyango20@gmail.com>
2017-05-03 18:18:04 +0300

    Greatest Number in an array formed by numbers entered by user.

--- Greatest.c
+++ Greatest.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 int main(){
-  int instances, i;
-  double numbers[100];
+  int instances, i, c, maximum;
+  int numbers[100];
   printf("Input the number of numbers you want to enter to find the greatest: ");
   scanf("%d", &instances);
-  for(i=0,i<=instances,++i){
-    numbers[i];
+  printf("Enter %d integers\n", instances);
+  for(i=0; i<instances; ++i){
+    scanf("%d", &numbers[i]);
   }
-  
+  maximum = numbers[0];
+  for (c = 1; c < instances; c++) {
+    if (numbers[c] > maximum) {
+      maximum  = numbers[c];
+    }
+  }
+  printf("The largest number is %d!\n", maximum);
   return 0;
 }

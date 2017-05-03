1866cc756c136e34e96e0cef312fdc1a602bc95f
Samwel Opiyo <samonyango20@gmail.com>
2017-05-03 15:30:23 +0300

    Pre to post

---
+++ Greatest.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+  int instances, i;
+  double numbers[100];
+  printf("Input the number of numbers you want to enter to find the greatest: ");
+  scanf("%d", &instances);
+  for(i=0,i<=instances,++i){
+    numbers[i];
+  }
+  
+  return 0;
+}
--- LCM_GCD.c
+++ LCM_GCD.c
@@ -6,12 +6,12 @@ int main(){
   printf("Input the second number: ");
   scanf("%d", &second_number);
   printf("You entered %d as your first number and %d as your second number!\n", first_number, second_number);
-  for(i=1; i <= first_number && i <= second_number; ++i) {
+  for(i=1; i <= first_number && i <= second_number; i++) {
     // Checks if i is factor of both integers
     if(first_number % i == 0 && second_number % i == 0)
     gcd = i;
   }
-  for(j=first_number*second_number; j >= first_number && j >= second_number ; --j) {
+  for(j=first_number*second_number; j >= first_number && j >= second_number ; j--) {
      // Checks if j is multiplier of both integers
      if(j % first_number == 0 && j % second_number == 0)
      lcm = j;

c7e50342119672fe86bd5e5844314416db1cdf5c
Samwel Opiyo <samonyango20@gmail.com>
2017-05-10 14:14:12 +0300

    C program to generate a right angled triangle with right angle on the left side bottom part

---
+++ Right_angle_Down_Left.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+  int columns, counter1, counter2;
+  printf("Input the Number of Columns: ");
+  scanf("%d", & columns);
+  for(counter1=1;counter1<=columns;counter1++){
+    for(counter2=1;counter2<=counter1;counter2++)printf("*");
+    printf("\n");
+  }
+  return 0;
+}
